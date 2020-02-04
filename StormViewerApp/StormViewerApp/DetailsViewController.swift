f3119b80fcbaf01a2c9693fd3166c4aa89a55b3f
Salma Salah <salma.salah@fawry.com>
2020-02-04 15:34:34 +0200

    to hide the upper bar that has the back button when tap on screen

--- StormViewerApp/StormViewerApp/DetailsViewController.swift
+++ StormViewerApp/StormViewerApp/DetailsViewController.swift
@@ -22,7 +22,7 @@ class DetailsViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        //to hide the upper bar thet has the back button when tap on screen
+        //to hide the upper bar that has the back button when tap on screen
         navigationController?.hidesBarsOnTap = true
     }
     
