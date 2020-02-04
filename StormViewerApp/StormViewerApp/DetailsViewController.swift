d42d9df421b3e73d457f08f7ab4d0a3109132f29
Salma Salah <salma.salah@fawry.com>
2020-02-04 14:59:19 +0200

    adding details screen

---
+++ StormViewerApp/StormViewerApp/DetailsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailsViewController.swift
+//  StormViewerApp
+//
+//  Created by Salma Salah on 2/4/20.
+//  Copyright © 2020 Salma Salah. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var detailsImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
